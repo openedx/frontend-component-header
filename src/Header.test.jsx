3e4a8a92454debbd98b929ad294b51762e7399c3
David Joy <djoy@edx.org>
2019-11-01 16:47:36 -0400

    feat: anonymous header works now
    Up until now we haven’t had any microfrontends that allowed anonymous usage.  The Header was encountering errors rendering in such situations, though it was close.  Now it properly shows the Login and Sign Up buttons.

--- src/Header.test.jsx
+++ src/Header.test.jsx
@@ -8,14 +8,39 @@ import { Context as ResponsiveContext } from 'react-responsive';
 import Header from './index';
 
 describe('<Header />', () => {
-  it('renders correctly for desktop', () => {
+  it('renders correctly for anonymous desktop', () => {
+    const component = (
+      <ResponsiveContext.Provider value={{ width: 1280 }}>
+        <IntlProvider locale="en" messages={{}}>
+          <AppContext.Provider value={{
+            authenticatedUser: null,
+            config: {
+              LMS_BASE_URL: process.env.LMS_BASE_URL,
+              SITE_NAME: process.env.SITE_NAME,
+              LOGIN_URL: process.env.LOGIN_URL,
+              LOGOUT_URL: process.env.LOGOUT_URL,
+            },
+            }}
+          >
+            <Header />
+          </AppContext.Provider>
+        </IntlProvider>
+      </ResponsiveContext.Provider>
+    );
+
+    const wrapper = TestRenderer.create(component);
+
+    expect(wrapper.toJSON()).toMatchSnapshot();
+  });
+
+  it('renders correctly for authenticated desktop', () => {
     const component = (
       <ResponsiveContext.Provider value={{ width: 1280 }}>
         <IntlProvider locale="en" messages={{}}>
           <AppContext.Provider value={{
             authenticatedUser: {
-              userId: null,
-              username: null,
+              userId: 'abc123',
+              username: 'edX',
               roles: [],
               administrator: false,
             },
@@ -38,14 +63,39 @@ describe('<Header />', () => {
     expect(wrapper.toJSON()).toMatchSnapshot();
   });
 
-  it('renders correctly for mobile', () => {
+  it('renders correctly for anonymous mobile', () => {
+    const component = (
+      <ResponsiveContext.Provider value={{ width: 500 }}>
+        <IntlProvider locale="en" messages={{}}>
+          <AppContext.Provider value={{
+            authenticatedUser: null,
+            config: {
+              LMS_BASE_URL: process.env.LMS_BASE_URL,
+              SITE_NAME: process.env.SITE_NAME,
+              LOGIN_URL: process.env.LOGIN_URL,
+              LOGOUT_URL: process.env.LOGOUT_URL,
+            },
+            }}
+          >
+            <Header />
+          </AppContext.Provider>
+        </IntlProvider>
+      </ResponsiveContext.Provider>
+    );
+
+    const wrapper = TestRenderer.create(component);
+
+    expect(wrapper.toJSON()).toMatchSnapshot();
+  });
+
+  it('renders correctly for authenticated mobile', () => {
     const component = (
       <ResponsiveContext.Provider value={{ width: 500 }}>
         <IntlProvider locale="en" messages={{}}>
           <AppContext.Provider value={{
             authenticatedUser: {
-              userId: null,
-              username: null,
+              userId: 'abc123',
+              username: 'edX',
               roles: [],
               administrator: false,
             },
