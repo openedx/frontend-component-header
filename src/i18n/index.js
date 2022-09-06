de9eb63b07b0672126c2f2d53593299fa7356070
Abdullah Waheed <abdullah.waheed@arbisoft.com>
2022-09-06 20:05:09 +0500

    feat: added new translations in Makefile and updated all the translations

--- src/i18n/index.js
+++ src/i18n/index.js
@@ -1,34 +1,28 @@
 import arMessages from './messages/ar.json';
-
-import caMessages from './messages/ca.json';
-import heMessages from './messages/he.json';
-import idMessages from './messages/id.json';
-import plMessages from './messages/pl.json';
-import ruMessages from './messages/ru.json';
-import thMessages from './messages/th.json';
-import ukMessages from './messages/uk.json';
-
-// no need to import en messages-- they are in the defaultMessage field
-import es419Messages from './messages/es_419.json';
 import frMessages from './messages/fr.json';
-import kokrMessages from './messages/ko_KR.json';
-import ptbrMessages from './messages/pt_BR.json';
+import es419Messages from './messages/es_419.json';
 import zhcnMessages from './messages/zh_CN.json';
+import ptMessages from './messages/pt.json';
+import itMessages from './messages/it.json';
+import ukMessages from './messages/uk.json';
+import deMessages from './messages/de.json';
+import ruMessages from './messages/ru.json';
+import hiMessages from './messages/hi.json';
+import frCAMessages from './messages/fr_CA.json';
+// no need to import en messages-- they are in the defaultMessage field
 
 const messages = {
   ar: arMessages,
-  ca: caMessages,
-  he: heMessages,
-  id: idMessages,
-  pl: plMessages,
-  ru: ruMessages,
-  th: thMessages,
-  uk: ukMessages,
   'es-419': es419Messages,
   fr: frMessages,
   'zh-cn': zhcnMessages,
-  'ko-kr': kokrMessages,
-  'pt-br': ptbrMessages,
+  pt: ptMessages,
+  it: itMessages,
+  de: deMessages,
+  hi: hiMessages,
+  'fr-ca': frCAMessages,
+  ru: ruMessages,
+  uk: ukMessages,
 };
 
 export default messages;
