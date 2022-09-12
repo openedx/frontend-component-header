5165a28f6855f93c66000ec5020d3b738a2d28f0
Bilal Qamar <59555732+BilalQamar95@users.noreply.github.com>
2022-09-12 15:44:57 +0500

    refactor: updated tests to accommodate jsx-no-constructed-context-values rule

--- src/StudioHeader.test.jsx
+++ src/StudioHeader.test.jsx
@@ -1,3 +1,4 @@
+/* eslint-disable react/prop-types */
 import React from 'react';
 import { IntlProvider } from '@edx/frontend-platform/i18n';
 import TestRenderer from 'react-test-renderer';
@@ -5,31 +6,37 @@ import { AppContext } from '@edx/frontend-platform/react';
 
 import { StudioHeader } from './index';
 
+function StudioHeaderComponent({ contextValue, appMenu = null, mainMenu = [] }) {
+  return (
+    <IntlProvider locale="en" messages={{}}>
+      <AppContext.Provider
+        value={contextValue}
+      >
+        <StudioHeader appMenu={appMenu} mainMenu={mainMenu} />
+      </AppContext.Provider>
+    </IntlProvider>
+  );
+}
+
 describe('<StudioHeader />', () => {
   it('renders correctly', () => {
-    const component = (
-      <IntlProvider locale="en" messages={{}}>
-        <AppContext.Provider
-          value={{
-            authenticatedUser: {
-              userId: 'abc123',
-              username: 'edX',
-              roles: [],
-              administrator: false,
-            },
-            config: {
-              STUDIO_BASE_URL: process.env.STUDIO_BASE_URL,
-              SITE_NAME: process.env.SITE_NAME,
-              LOGIN_URL: process.env.LOGIN_URL,
-              LOGOUT_URL: process.env.LOGOUT_URL,
-              LOGO_URL: process.env.LOGO_URL,
-            },
-          }}
-        >
-          <StudioHeader />
-        </AppContext.Provider>
-      </IntlProvider>
-    );
+    const contextValue = {
+      authenticatedUser: {
+        userId: 'abc123',
+        username: 'edX',
+        roles: [],
+        administrator: false,
+      },
+      config: {
+        STUDIO_BASE_URL: process.env.STUDIO_BASE_URL,
+        SITE_NAME: process.env.SITE_NAME,
+        LOGIN_URL: process.env.LOGIN_URL,
+        LOGOUT_URL: process.env.LOGOUT_URL,
+        LOGO_URL: process.env.LOGO_URL,
+      },
+    };
+
+    const component = <StudioHeaderComponent contextValue={contextValue} />;
 
     const wrapper = TestRenderer.create(component);
 
@@ -57,29 +64,22 @@ describe('<StudioHeader />', () => {
         },
       ],
     };
-    const component = (
-      <IntlProvider locale="en" messages={{}}>
-        <AppContext.Provider
-          value={{
-            authenticatedUser: {
-              userId: 'abc123',
-              username: 'edX',
-              roles: [],
-              administrator: false,
-            },
-            config: {
-              STUDIO_BASE_URL: process.env.STUDIO_BASE_URL,
-              SITE_NAME: process.env.SITE_NAME,
-              LOGIN_URL: process.env.LOGIN_URL,
-              LOGOUT_URL: process.env.LOGOUT_URL,
-              LOGO_URL: process.env.LOGO_URL,
-            },
-          }}
-        >
-          <StudioHeader appMenu={appMenu} />
-        </AppContext.Provider>
-      </IntlProvider>
-    );
+    const contextValue = {
+      authenticatedUser: {
+        userId: 'abc123',
+        username: 'edX',
+        roles: [],
+        administrator: false,
+      },
+      config: {
+        STUDIO_BASE_URL: process.env.STUDIO_BASE_URL,
+        SITE_NAME: process.env.SITE_NAME,
+        LOGIN_URL: process.env.LOGIN_URL,
+        LOGOUT_URL: process.env.LOGOUT_URL,
+        LOGO_URL: process.env.LOGO_URL,
+      },
+    };
+    const component = <StudioHeaderComponent contextValue={contextValue} appMenu={appMenu} />;
 
     const wrapper = TestRenderer.create(component);
 
@@ -104,29 +104,22 @@ describe('<StudioHeader />', () => {
         content: 'Content 3',
       },
     ];
-    const component = (
-      <IntlProvider locale="en" messages={{}}>
-        <AppContext.Provider
-          value={{
-            authenticatedUser: {
-              userId: 'abc123',
-              username: 'edX',
-              roles: [],
-              administrator: false,
-            },
-            config: {
-              STUDIO_BASE_URL: process.env.STUDIO_BASE_URL,
-              SITE_NAME: process.env.SITE_NAME,
-              LOGIN_URL: process.env.LOGIN_URL,
-              LOGOUT_URL: process.env.LOGOUT_URL,
-              LOGO_URL: process.env.LOGO_URL,
-            },
-          }}
-        >
-          <StudioHeader mainMenu={mainMenu} />
-        </AppContext.Provider>
-      </IntlProvider>
-    );
+    const contextValue = {
+      authenticatedUser: {
+        userId: 'abc123',
+        username: 'edX',
+        roles: [],
+        administrator: false,
+      },
+      config: {
+        STUDIO_BASE_URL: process.env.STUDIO_BASE_URL,
+        SITE_NAME: process.env.SITE_NAME,
+        LOGIN_URL: process.env.LOGIN_URL,
+        LOGOUT_URL: process.env.LOGOUT_URL,
+        LOGO_URL: process.env.LOGO_URL,
+      },
+    };
+    const component = <StudioHeaderComponent contextValue={contextValue} mainMenu={mainMenu} />;
 
     const wrapper = TestRenderer.create(component);
 
