4ce7311809abb708f2ac9d431189e5036871464d
SundasNoreen <sundas.noreen@arbisoft.com>
2023-05-22 15:17:04 +0500

    refactor: removed unused states

--- src/constants.js
+++ src/constants.js
@@ -1,8 +1,3 @@
-export const IDLE_STATUS = 'idle';
-export const LOADING_STATUS = 'loading';
-export const SUCCESS_STATUS = 'success';
-export const FAILURE_STATUS = 'failure';
-
 export const notificationTabs = {
   REMINDERS: 'reminders',
   DISCUSSIONS: 'discussions',
