9a2964fb4c6e59e80a0d5bf25febb0c98f00eb00
Farhaan Bukhsh <farhaan@opencraft.com>
2025-10-02 17:27:39 +0530

    fixup! feat: Adds notification tray slot

--- src/plugin-slots/NotificationSlot/index.jsx
+++ src/plugin-slots/NotificationSlot/index.jsx
@@ -4,7 +4,7 @@ import { PluginSlot } from '@openedx/frontend-plugin-framework';
 const NotificationTraySlot = () => (
   <PluginSlot
     id="org.openedx.frontend.layout.notification_tray.v1"
-    idAliases={["notification_tray_plugin"]}
+    idAliases={['notification_tray_plugin']}
   />
 );
 
