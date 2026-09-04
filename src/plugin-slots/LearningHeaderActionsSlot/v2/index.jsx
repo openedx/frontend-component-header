daa487ec18ec7237c419db7e751ce6ba310277b3
XnpioChV <xnpiochv@gmail.com>
2026-09-03 20:43:51 -0500

    fix: Nits on the code

--- src/plugin-slots/LearningHeaderActionsSlot/v2/index.jsx
+++ src/plugin-slots/LearningHeaderActionsSlot/v2/index.jsx
@@ -3,9 +3,10 @@ import PropTypes from 'prop-types';
 import { PluginSlot } from '@openedx/frontend-plugin-framework';
 import LearningHeaderActionsSlotV1 from '../v1';
 
-const LearningHeaderActionsSlot = ({ showDefaultActions }) => (
+const LearningHeaderActionsSlot = ({ showDefaultActions = true }) => (
   <PluginSlot
     id="org.openedx.frontend.layout.learning_header_actions.v2"
+    pluginProps={{ showDefaultActions }}
   >
     {showDefaultActions && <LearningHeaderActionsSlotV1 />}
   </PluginSlot>
@@ -13,10 +14,6 @@ const LearningHeaderActionsSlot = ({ showDefaultActions }) => (
 
 LearningHeaderActionsSlot.propTypes = {
   showDefaultActions: PropTypes.bool,
-};
-
-LearningHeaderActionsSlot.defaultProps = {
-  showDefaultActions: true,
 };
 
 export default LearningHeaderActionsSlot;
