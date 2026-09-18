5fcfa39de92382506c61ac67a907272066ef3c0b
XnpioChV <xnpiochv@gmail.com>
2026-09-18 16:43:30 -0500

    fix: only warn once when v1 slot has custom pluginSlots config

--- src/plugin-slots/LearningHeaderActionsSlot/v1/index.jsx
+++ src/plugin-slots/LearningHeaderActionsSlot/v1/index.jsx
@@ -1,21 +1,29 @@
 import React from 'react';
+import { getConfig } from '@edx/frontend-platform';
 import { PluginSlot } from '@openedx/frontend-plugin-framework';
 import HeaderNotificationsSlot from '../../HeaderNotificationsSlot';
 import LearningHelpSlot from '../../LearningHelpSlot';
 
+const SLOT_ID = 'org.openedx.frontend.layout.learning_header_actions.v1';
+
+let hasWarned = false;
+
 /** @deprecated Use `LearningHeaderActionsSlotV2` instead. See DEPR ticket openedx/frontend-component-header#681. */
 const LearningHeaderActionsSlotV1 = () => {
-  // eslint-disable-next-line no-console
-  console.warn(
-    '[DEPRECATED] The "org.openedx.frontend.layout.learning_header_actions.v1" plugin slot '
-    + 'is deprecated and will be removed. Migrate your pluginSlots config to '
-    + '"org.openedx.frontend.layout.learning_header_actions.v2". '
-    + 'See https://github.com/openedx/frontend-component-header/issues/681 for details.',
-  );
+  if (!hasWarned && getConfig()?.pluginSlots?.[SLOT_ID]) {
+    hasWarned = true;
+    // eslint-disable-next-line no-console
+    console.warn(
+      `[Deprecated] The "${SLOT_ID}" plugin slot `
+      + 'is deprecated and will be removed. Migrate your pluginSlots config to '
+      + '"org.openedx.frontend.layout.learning_header_actions.v2". '
+      + 'See https://github.com/openedx/frontend-component-header/issues/681 for details.',
+    );
+  }
 
   return (
     <PluginSlot
-      id="org.openedx.frontend.layout.learning_header_actions.v1"
+      id={SLOT_ID}
     >
       <HeaderNotificationsSlot />
       <LearningHelpSlot />
